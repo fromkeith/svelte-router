a7df472ca5d55aed076e9abfcd365a232bd82dfc
fromkeith <fromkeith@users.noreply.github.com>
2023-01-25 15:21:03 -0500

    update typescript defs to export activeRoute

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,8 +3,9 @@ import { SpaRouter } from './spa_router';
 import { localisedRoute } from './spa_router';
 import { navigateTo } from './spa_router';
 import { routeIsActive } from './spa_router';
+import { activeRoute } from './store';
 import Route from './components/route';
 import Router from './components/router';
 import Navigate from './components/navigate';
 
-export { SpaRouter, localisedRoute, navigateTo, routeIsActive, Route, Router, Navigate };
+export { SpaRouter, localisedRoute, navigateTo, routeIsActive, Route, Router, Navigate, activeRoute };
